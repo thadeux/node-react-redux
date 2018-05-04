76dc63a0eb31b2e797b3983a4399c769b80df0eb
rmarshall <rob.h.marshall@gmail.com>
2018-05-04 14:26:32 -0400

    check env for port

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
     res.send('Hello World');
 });
 
-app.listen(443);
+app.listen(port);
