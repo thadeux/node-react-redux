2c9261cf1f6de2f727c1188cb9263d8bd5e2dcb9
rmarshall <rob.h.marshall@gmail.com>
2018-05-04 14:46:04 -0400

    serve static files

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(port);
+
+console.log("listening on port %d", port);
