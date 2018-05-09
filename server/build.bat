4f355cfcecbad36413df0ac8aefe33c372210e3c
rmarshall <rob.h.marshall@gmail.com>
2018-05-09 07:16:29 -0400

    changed xcopy method

--- server/build.bat
+++ server/build.bat
@@ -1,4 +1,4 @@
-xcopy server\index.html dist\index.html* /Y /E
-xcopy server\process.json dist\process.json* /Y /E
-xcopy server\server.js dist\server.js* /Y /E
-xcopy server\web.config dist\web.config* /Y /E
+xcopy server\index.html dist\ /Y /E
+xcopy server\process.json dist\ /Y /E
+xcopy server\server.js dist\ /Y /E
+xcopy server\web.config dist\ /Y /E
