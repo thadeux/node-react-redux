5dc665602cc2491eb8758489ee7d5ad17a811cd1
rmarshall <rob.h.marshall@gmail.com>
2018-05-21 18:08:43 -0400

    add nodemon, use static serve

--- server/server.js
+++ server/server.js
@@ -4,9 +4,12 @@ var path = require('path');
 
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
+app.use(express.static(__dirname));
+/*app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+*/
+
 
 app.listen(port);
 
